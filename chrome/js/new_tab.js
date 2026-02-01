7bb1a24f808f390f410738bb722453a577c10919
Matsu <matsu3x9@icloud.com>
2026-02-02 03:53:42 +0800

    Mon Feb  2 03:53:42 CST 2026

--- chrome/js/new_tab.js
+++ chrome/js/new_tab.js
@@ -1,27 +1,21 @@
-'use strict';
+"use strict";
 
-const dark_theme_media_query = window.matchMedia(
-  '(prefers-color-scheme: light)'
-);
-const icon_sizes = [16, 24, 32, 48, 64, 128];
+const themeMedia = window.matchMedia("(prefers-color-scheme: light)");
 
 const set_title = () => {
-  document.querySelector('title').textContent =
-    chrome.i18n.getMessage('new_tab_title_text');
+    document.title = "New Tab";
 };
 
 const set_icon = () => {
-  const icon_els = document.querySelectorAll('.icon');
+    const icon = document.querySelector(".icon");
+    if (!icon) return;
 
-  icon_sizes.forEach((icon_size, i) => {
-    const icon_el = icon_els[i];
-    const theme = dark_theme_media_query.matches ? 'light' : 'dark';
-
-    icon_el.href = `../icons/icon_${theme + icon_size}.png`;
-  });
+    icon.href = themeMedia.matches
+        ? "../icons/icon_light128.png"
+        : "../icons/icon_dark128.png";
 };
 
 set_title();
 set_icon();
 
-dark_theme_media_query.addEventListener('change', set_icon);
+themeMedia.addEventListener("change", set_icon);
